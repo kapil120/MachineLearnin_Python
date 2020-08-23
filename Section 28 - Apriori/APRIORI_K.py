efa45f019d2b96d776552df5d04725f5f4d43c9e
kapil120 <sharma.kapil1208@gmail.com>
2020-08-23 10:22:33 +0530

    Add files via upload

---
+++ Section 28 - Apriori/APRIORI_K.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 26 12:17:59 2019
+
+@author: Kapil
+"""
+
+#Apriori
+
+#Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Importing the dataset
+dataset=pd.read_csv('Market_Basket_Optimisation.csv',header=None)
+transactions=[]
+for i in range(0,7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0,20)])
+
+#Training Apriori on the dataset
+from apyori import apriori
+rules=apriori(transactions,min_support=0.003,min_confidence=0.2,min_lift=3,min_length=2)
+
+#Visulising the Results
+result=list(rules)
